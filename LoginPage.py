6879aef128b48ea8f6e854594fd4fb99271950e5
MarcusLSC <marcuslsc0816@gmail.com>
2024-01-07 01:05:53 +0800

    initial commit

---
+++ LoginPage.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template
+LoginPage = Flask(__name__)
+@LoginPage.route('/')
+
+def loadLogin():
+    return render_template("index.html")
+# add functions for login and cookies for session
+
+if __name__ == '__main__':
+    LoginPage.run(port = 5000)
---
+++ loadSInfo.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template
+StudentInfoPage = Flask(__name__)
+@StudentInfoPage.route('/')
+
+def loadSInfoPage():
+    return render_template("sinfo.html")
+
+
+if __name__ == '__main__':
+    loadSInfoPage.run(port = 5002)
