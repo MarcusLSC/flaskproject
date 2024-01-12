9647e49cb3f32e0cbf1f548e172e784763975bfd
MarcusLSC <marcuslsc0816@gmail.com>
2024-01-12 21:59:58 +0800

    tweaked mainpage.py

--- MainPage.py
+++ MainPage.py
@@ -1,10 +1,27 @@
 from flask import Flask, render_template
-MainPage = Flask(__name__)
-@MainPage.route('/')
-
+app = Flask(__name__)
+@app.route('/')
 def loadmain():
     #retrieve user data from db and put username on the mainpage
     return render_template("mainpage.html")
 
+@app.route('/sinfo.html')
+def loadSInfoPage():
+    return render_template("sinfo.html")
+
+@app.route('/scoreentry.html')
+def loadScoreEntryPage():
+    return render_template("scoreentry.html")
+
+@app.route('/subject.html')
+def loadSubjectPage():
+    return render_template("subject.html")
+
+
+
+@app.route('/statistics.html')
+def loadGenReportPage():
+    return render_template("statistics.html")
+
 if __name__ == '__main__':
-    MainPage.run(port=5001)
+    app.run(port=5000)
